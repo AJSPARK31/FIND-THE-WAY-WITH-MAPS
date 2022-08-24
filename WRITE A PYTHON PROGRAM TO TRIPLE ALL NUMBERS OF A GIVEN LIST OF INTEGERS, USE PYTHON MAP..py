902797bbeb05ede59c719c5e4ab59f9947fc5e00
AJSPARK31 <88500955+AJSPARK31@users.noreply.github.com>
2022-08-24 15:53:08 +0530

    Add files via upload

---
+++ WRITE A PYTHON PROGRAM TO TRIPLE ALL NUMBERS OF A GIVEN LIST OF INTEGERS, USE PYTHON MAP..py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+# Write a Python program to triple all numbers of a given list of integers. Use Python map.
+
+
+
+
+my_list=[1, 2, 3, 4, 5, 6, 7]
+result=list(map(lambda x:x*3,my_list))
+print(result)
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
